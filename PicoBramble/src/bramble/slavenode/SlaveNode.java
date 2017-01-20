e1ad29b8fbebf192c570139c892d677ce1f8605e
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-01-20 22:20:09 +0000

    Add javadoc

--- PicoBramble/src/bramble/slavenode/SlaveNode.java
+++ PicoBramble/src/bramble/slavenode/SlaveNode.java
@@ -19,7 +19,7 @@ public abstract class SlaveNode extends GenericNode implements Runnable {
 	private int jobID;
 	private ArrayList<Serializable> initializationData;
 	
-	public SlaveNode() {
+	protected SlaveNode() {
 		super();
 		
 		try {
@@ -31,13 +31,23 @@ public abstract class SlaveNode extends GenericNode implements Runnable {
 		}
 	}
 
+	/**
+	 * Will call listen() forever, meaning 
+	 * that more than one job can be scheduled.
+	 */
 	public void listenForever() {
-		
 		while(true){
 			listen();
 		}
 	}
 	
+	/**
+	 * Sets up a listener server to listen for incoming jobs.
+	 * 
+	 * When a job is received, it sets jobID and 
+	 * initializationData fields in this class, then calls run().
+	 * 
+	 */
 	public void listen(){
 		try {
 			JobSetupData jobSetupData = (JobSetupData) listenerServer.listen();
@@ -45,7 +55,6 @@ public abstract class SlaveNode extends GenericNode implements Runnable {
 			this.jobID = jobSetupData.getJobID();
 			this.initializationData = jobSetupData.getInitializationData();
 			
-			// Parse in seperate thread to avoid missing packet(s).
 			new Thread(this).start();
 			
 		} catch (ClassNotFoundException | IOException e) {
@@ -70,10 +79,29 @@ public abstract class SlaveNode extends GenericNode implements Runnable {
 		}
 	}
 	
+	/**
+	 * Called when a job starts. By default, will 
+	 * call runJob with the jobID and initialization 
+	 * data as parameters.
+	 * 
+	 * Clients should override runJob() rather than run().
+	 */
 	public void run(){
 		runJob(this.jobID, this.initializationData);
 	}
 	
+	/**
+	 * Clients should override this method to call code relevant to their project.
+	 * 
+	 * @param jobID - the job identifier. This 
+	 * 					should be saved and sent back to the master 
+	 * 					node along with any relevant computation 
+	 * 					data once the job is finished.
+	 * 
+	 * @param initializationData - An array of data which is used to 
+	 * 							set the initial state, configuration, 
+	 * 							or limits of a computation.
+	 */
 	protected abstract void runJob(int jobID, ArrayList<Serializable> initializationData);
 
 }
