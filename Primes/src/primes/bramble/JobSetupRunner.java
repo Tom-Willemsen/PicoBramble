c92ba4f1f48c4a97ab6453858921ce76c2cd3dc9
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-01-21 12:12:53 +0000

    Refactor number of job requests

--- Primes/src/primes/bramble/JobSetupRunner.java
+++ Primes/src/primes/bramble/JobSetupRunner.java
@@ -7,19 +7,18 @@ import bramble.masternode.JobSetup;
 import bramble.networking.JobSetupData;
 
 public class JobSetupRunner extends JobSetup {
+	
+	int jobsRequested = 0;
 
 	@Override
 	public JobSetupData getJobSetupData() {
 		
 		ArrayList<Serializable> init2 = new ArrayList<Serializable>();
-		init2.add(new Long(10000000));
-		init2.add(new Long(20000000));
+		init2.add(new Long(jobsRequested*10000000));
+		init2.add(new Long((jobsRequested+1)*10000000));
 		
+		this.jobsRequested++;
 		return new JobSetupData(1, init2);
-		
-	}
-	
-	public static void addSlaveNode(){
 		
 	}
 
