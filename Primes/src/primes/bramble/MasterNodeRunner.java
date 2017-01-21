b1299651ccd5a2e1a1e2573ce9cab44929b02832
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-01-21 00:16:05 +0000

    Combine parser and masternode back into one class

--- Primes/src/primes/bramble/MasterNodeRunner.java
+++ Primes/src/primes/bramble/MasterNodeRunner.java
@@ -1,12 +1,48 @@
 package primes.bramble;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import bramble.masternode.MasterNode;
+import bramble.networking.JobResponseData;
+import bramble.networking.JobSetupData;
 
 public class MasterNodeRunner extends MasterNode {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Serializable> init2 = new ArrayList<Serializable>();
+		init2.add(new Long(10000000));
+		init2.add(new Long(20000000));
+		
+		try {
+			(new JobSetupData(1, init2)).send();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ArrayList<Serializable> init3 = new ArrayList<Serializable>();
+		init3.add(new Long(20000000));
+		init3.add(new Long(30000000));
+		
+		try {
+			(new JobSetupData(2, init3)).send();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args){			
+		(new MasterNodeRunner()).listenForever();	
+	}
+	
+	@Override
+	protected void parse(JobResponseData jobResponseData) {
+		System.out.println("Job [" + jobResponseData.getJobIdentifier() + "] replied with "
+				+ jobResponseData.getData().size() + " primes.");
 		
 	}
 
