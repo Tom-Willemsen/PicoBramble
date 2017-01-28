bc7392d81344372be35ac73a196043f7166a7568
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-01-28 18:32:38 +0000

    Add a basic web API layer

---
+++ PicoBramble/src/bramble/webserver/WebAPI.java
@@ -0,0 +1,20 @@
+package bramble.webserver;
+
+import bramble.controllernode.ControllerNode;
+
+public final class WebAPI {
+
+	private static ControllerNode controllerNode;
+	
+	synchronized public final static void registerControllerNode(ControllerNode controllerNode){
+		WebAPI.controllerNode = controllerNode;
+	}
+	
+	public static final int getFreeJobSlots(){
+		try{
+			return controllerNode.getJobSlotsAvailable();
+		} catch (NullPointerException e){
+			return 0;
+		}
+	}
+}
