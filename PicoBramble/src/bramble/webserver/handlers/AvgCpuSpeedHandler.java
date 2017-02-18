c3d3d162217a5844f36a1f96a9615e9d2fcfd649
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-18 12:37:46 +0000

    Refactor web API

--- PicoBramble/src/bramble/webserver/handlers/AvgCpuSpeedHandler.java
+++ PicoBramble/src/bramble/webserver/handlers/AvgCpuSpeedHandler.java
@@ -1,25 +1,13 @@
 package bramble.webserver.handlers;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.http.HttpStatus;
-
 import bramble.webserver.WebAPI;
 
-public class AvgCpuSpeedHandler extends HttpServlet {
+public class AvgCpuSpeedHandler extends BaseHandler {
+	
 	private static final long serialVersionUID = 5414855470057516723L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-
-		resp.addHeader("Access-Control-Allow-Origin", "*");
-		resp.setStatus(HttpStatus.OK_200);
-		resp.getWriter().print(WebAPI.getAvgClusterCpuSpeed());
+	protected String getData() {
+		return WebAPI.getAvgClusterCpuSpeed();
 	}
 }
--- PicoBramble/src/bramble/webserver/handlers/TotalJobSlotsHandler.java
+++ PicoBramble/src/bramble/webserver/handlers/TotalJobSlotsHandler.java
@@ -1,26 +1,13 @@
 package bramble.webserver.handlers;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.http.HttpStatus;
-
 import bramble.webserver.WebAPI;
 
-public class TotalJobSlotsHandler extends HttpServlet {
+public class TotalJobSlotsHandler extends BaseHandler {
 
 	private static final long serialVersionUID = 5414855470057516723L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-
-		resp.addHeader("Access-Control-Allow-Origin", "*");
-		resp.setStatus(HttpStatus.OK_200);
-		resp.getWriter().print(WebAPI.getTotalJobSlots());
+	protected String getData() {
+		return Integer.toString(WebAPI.getTotalJobSlots());
 	}
 }
