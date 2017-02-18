f18bc870a3f072b87c6e6139d37523ddc8b7b663
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-18 19:30:36 +0000

    Fix checkstyle warnings

--- ExampleApps/Primes/src/primes/PrimalityTests.java
+++ ExampleApps/Primes/src/primes/PrimalityTests.java
@@ -9,29 +9,29 @@ public final class PrimalityTests {
 	/**
 	 * Tests a number for being a prime.
 	 * 
-	 * @param n - the number to test
+	 * @param number - the number to test
 	 * @return true if the number is a prime, false otherwise
 	 */
-	public static boolean isPrime(Long n){
+	public static boolean isPrime(Long number){
 
-		return test_tiny_primes(n) && test_divisors(n);
+		return test_tiny_primes(number) && test_divisors(number);
 	}
 	
 	/**
 	 * Tests for a prime for n<10 by comparing to a list of known primes
-	 * @param n - an integer less than 10.
+	 * @param number - an integer less than 10.
 	 * @return true if the number is a prime, false otherwise
 	 */
 	
-	private static boolean test_tiny_primes(Long n){
-		if(n%2L == 0 || n%3L == 0|| n%5L == 0 || n%7L == 0 || n < 0){
+	private static boolean test_tiny_primes(Long number){
+		if(number%2L == 0 || number%3L == 0|| number%5L == 0 || number%7L == 0 || number < 0){
 			return false;
 		}
 		return true;
 	}
 	
 	/**
-	 *  Tests for a prime by an exhaustive search for factors
+	 *  Tests for a prime by an exhaustive search for factors.
 	 *  
 	 *  @return true if the number is a prime, false otherwise
 	 *  
@@ -39,14 +39,14 @@ public final class PrimalityTests {
 	 *  - Only check factors up to sqrt(n)
 	 *  - Only check for factors 2, 3, 6k-1 and 6k+1 (integer k)
 	 */
-	private static boolean test_divisors(Long n){
+	private static boolean test_divisors(Long number){
 		
-		final Long square_root_n = Long.valueOf((long) Math.ceil(Math.sqrt(n)));
+		final Long square_root_n = Long.valueOf((long) Math.ceil(Math.sqrt(number)));
 		
 		// Can start at i=2 because 5 & 7 are covered by check_tiny_primes()
 		for(Long i = Long.valueOf(2); (SIX*i)-ONE<=square_root_n; i++){
 			
-			if(n%(SIX*i - ONE) == ZERO || n%(SIX*i + ONE) == ZERO){
+			if(number%(SIX*i - ONE) == ZERO || number%(SIX*i + ONE) == ZERO){
 				return false;
 			}
 
