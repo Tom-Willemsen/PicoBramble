6b35ccb4fbc0eef497ca436e2dcbb1dfa0e8f9f4
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-19 15:03:10 +0000

    Fix checkstyle warnings in bramble.node.slave

--- PicoBramble/src/bramble/node/slave/ISlaveNodeRunner.java
+++ PicoBramble/src/bramble/node/slave/ISlaveNodeRunner.java
@@ -8,14 +8,11 @@ public interface ISlaveNodeRunner{
     /**
      * Clients should override this method to call code relevant to their project.
      * 
-     * @param jobIdentifier - the job identifier. This 
-     * 			should be saved and sent back to the master 
-     * 			node along with any relevant computation 
-     * 			data once the job is finished.
+     * @param jobIdentifier - the job identifier. This should be saved and sent back to 
+     * the master node along with any relevant computation data once the job is finished.
      * 
-     * @param initializationData - An array of data which is used to 
-     * 					set the initial state, configuration, 
-     * 					or limits of a computation.
+     * @param initializationData - An array of data which is used to set the initial state, 
+     * configuration, or limits of a computation.
      */
     public void runJob(int jobIdentifier, Collection<Serializable> initializationData);
 
