0aba42dc26f5816cd194e21833cf6008e8d25e77
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-19 14:49:48 +0000

    Fix checkstyle warnings in bramble.node.master

--- PicoBramble/src/bramble/node/master/IMasterNodeRunner.java
+++ PicoBramble/src/bramble/node/master/IMasterNodeRunner.java
@@ -7,8 +7,8 @@ public interface IMasterNodeRunner {
 	/**
 	 * Parses job response data. 
 	 * 
-	 * This method could pass the data to an analysis class, 
-	 * or simply save it to a file for future inspection.
+	 * <p>This method could pass the data to an analysis class, 
+	 * or simply save it to a file for future inspection.</p>
 	 * 
 	 * @param jobResponseData - The jobResponseData to be parsed.
 	 */
--- PicoBramble/src/bramble/node/master/MasterNode.java
+++ PicoBramble/src/bramble/node/master/MasterNode.java
@@ -1,104 +1,125 @@
 package bramble.node.master;
 
 import java.io.IOException;
-import java.net.BindException;
-
 import bramble.configuration.BrambleConfiguration;
 import bramble.networking.ListenerServer;
 import bramble.networking.Message;
 import bramble.node.manager.IManager;
 
 public class MasterNode implements Runnable {
-	
-	private final ListenerServer listenerServer;
-	private MessageParser messageParser;
-	private IManager manager;
-	
-	/**
-	 * Constructor.
-	 * @param masterNodeRunner the master node runner to use
-	 * @throws IOException 
-	 * @throws BindException 
-	 */
-	public MasterNode(IManager manager, IMasterNodeRunner masterNodeRunner) throws IOException{		
-		this(manager, masterNodeRunner, new ListenerServer(BrambleConfiguration.MASTER_PORT));
+
+    private final ListenerServer listenerServer;
+    private MessageParser messageParser;
+    private IManager manager;
+
+    /**
+     * Constructor.
+     * @param manager
+     * 	- the manager of this master node
+     * @param masterNodeRunner 
+     * 	- the master node runner to use
+     * @throws IOException 
+     * 	- if there was an error setting up the master node
+     */
+    public MasterNode(IManager manager, 
+	    IMasterNodeRunner masterNodeRunner) throws IOException{	
+
+	this(manager, masterNodeRunner, 
+		new ListenerServer(BrambleConfiguration.MASTER_PORT));
+    }
+
+    /**
+     * Constructor, specifying a listener server.
+     * @param manager
+     * 	- the manager of this master node
+     * @param masterNodeRunner 
+     * 	- the master node runner to use
+     * @param listenerServer
+     * 	- specify a custom listener server
+     */
+    public MasterNode(IManager manager, 
+	    IMasterNodeRunner masterNodeRunner, ListenerServer listenerServer){
+
+	this(manager, listenerServer, 
+		new MessageParser(manager, masterNodeRunner));
+    }
+
+    /**
+     * Constructor, specifying a listener server and message parser.
+     * @param manager
+     * 	- the manager of this master node
+     * @param listenerServer
+     * 	- specify a custom listener server
+     * @param messageParser
+     * 	- specify a custom message parser
+     */
+    public MasterNode(IManager manager, 
+	    ListenerServer listenerServer, MessageParser messageParser){
+
+	this.listenerServer = listenerServer;
+	this.messageParser = messageParser;
+	this.manager = manager;
+    }
+
+    /**
+     * Listen for a single message.
+     * @param listenerServer - the listener server to listen to
+     */
+    public void listen(ListenerServer listenerServer){
+	try {
+	    Message data = listenerServer.listen();	
+	    if(data != null){
+		parseIncomingData(data);
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	/**
-	 * Constructor, specifying a listener server.
-	 */
-	public MasterNode(IManager manager, IMasterNodeRunner masterNodeRunner, ListenerServer listenerServer){
-		this(manager, listenerServer, new MessageParser(manager, masterNodeRunner));
+    }
+
+    /**
+     * Keep listening for messages from slave nodes.
+     * 
+     * <p>Note: This is a blocking method!</p>
+     */
+    private void listenForever() {
+	while(true){
+	    listen(listenerServer);
+	    try {
+		Thread.sleep(BrambleConfiguration.LISTENER_DELAY_MS);
+	    } catch (InterruptedException e) {
+		return;
+	    }
 	}
-	
-	/**
-	 * Constructor, specifying a listener server and message parser.
-	 */
-	public MasterNode(IManager manager, ListenerServer listenerServer, MessageParser messageParser){
-		this.listenerServer = listenerServer;
-		this.messageParser = messageParser;
-		this.manager = manager;
-	}
-	
-	/**
-	 * Listen for a single message.
-	 * @param listenerServer
-	 */
-	public void listen(ListenerServer listenerServer){
-		try {
-			Message data = listenerServer.listen();	
-			if(data != null){
-				parseIncomingData(data);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+    }
+
+    /**
+     * Parses incoming data in a new thread.
+     * 
+     * @param incomingData - the data to be parsed
+     */
+    private void parseIncomingData(final Message incomingData){	
+	manager.execute(new Runnable(){
+	    public void run(){
+		try{
+		    messageParser.parse(incomingData);
+		} catch (IllegalArgumentException e){
+		    return;
 		}
-	}
-	
-	/**
-	 * Keep listening for messages from slave nodes.
-	 * 
-	 * Note: This is a blocking method!
-	 */
-	private void listenForever() {
-		while(true){
-			listen(listenerServer);
-			try {
-				Thread.sleep(BrambleConfiguration.LISTENER_DELAY_MS);
-			} catch (InterruptedException e) {
-				return;
-			}
-		}
-	}
-		
-	/**
-	 * Parses incoming data in a new thread.
-	 * 
-	 * @param incomingData - the data to be parsed
-	 */
-	private void parseIncomingData(final Message incomingData){	
-		manager.execute(new Runnable(){
-			public void run(){
-				try{
-					messageParser.parse(incomingData);
-				} catch (IllegalArgumentException e){
-					return;
-				}
-			}
-		});
-	}
-	
-	/**
-	 * Listens for job response data forever.
-	 */
-	@Override
-	public final void run() {
-		manager.execute(new Runnable(){
-			@Override
-			public void run(){
-				listenForever();
-			}
-		});
-	}
-	
+	    }
+	});
+    }
+
+    /**
+     * Listens for job response data forever.
+     */
+    @Override
+    public final void run() {
+	manager.execute(new Runnable(){
+	    @Override
+	    public void run(){
+		listenForever();
+	    }
+	});
+    }
+
 }
