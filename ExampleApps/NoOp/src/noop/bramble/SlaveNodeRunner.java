3827cd5ea5a7f6d353b8990ba33ad70b4f34c560
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-24 20:32:16 +0000

    Fix checkstyle

--- ExampleApps/NoOp/src/noop/bramble/SlaveNodeRunner.java
+++ ExampleApps/NoOp/src/noop/bramble/SlaveNodeRunner.java
@@ -48,7 +48,8 @@ public class SlaveNodeRunner implements ISlaveNodeRunner {
 	@Override
 	public void runJob(int jobId, Collection<Serializable> initializationData) {	
 		try {
-			slaveNode.sendData(jobId, initializationData.toString(), initializationData);
+			slaveNode.sendData(jobId, 
+					initializationData.toString(), initializationData);
 		} catch (IOException e) {
 			LogManager.getLogger().error("Couldn't send data.", e);
 			return;
